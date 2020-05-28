e4d870440b9edf1540c8e91c4e28674fc7cca5f2
kshitizified <kshitizkumar1995@gmail.com>
2020-05-28 17:50:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const exphbs  = require('express-handlebars');
+
+const PORT = process.env.PORT || 5000;
+
+// setting epress-handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set Handlebars routes
+app.get('/', function (req, res) {
+    res.render('home',{
+        stuff : "This is stuff"
+    });
+});
+
+// setting static folders
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log("Server Listening on Port: "+PORT));
