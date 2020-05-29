01601a038a0c13197f56b648bab412d927bf5861
kshitizified <kshitizkumar1995@gmail.com>
2020-05-29 22:41:34 +0530

    updating index.js with async API calls and refactoring views

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
 // Stock Market Portfolio App by Kshitiz Kumar
 
 const express = require('express');
-const app = express();
 const path = require('path');
 const exphbs  = require('express-handlebars');
-const reqest = require('request');
+const request = require('request');
 const bodyParser = require('body-parser');
+const async = require('async');
+
 const PORT = process.env.PORT || 5000;
 
 // API KEY pk_6cad451b2ea543e4aa1b534419d7489f
 
 
 // use body-parser middleware
+const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
-
-function call_stock_api(doneCallingAPI,ticker) {
-    console.log(ticker);
-    if(ticker==undefined){ ticker='goog'};
-    reqest('https://cloud.iexapis.com/stable/stock/'+ticker+'/quote?token=pk_6cad451b2ea543e4aa1b534419d7489f', 
-        { json : true },
-        (err, res, body) => {
-            if(err) { return console.log(err);}
-            if(res.statusCode === 200){
-                doneCallingAPI(body);
-            };
-    });
-}
-
-// function call_company_api(doneCallingCompany) {
-//     reqest('https://cloud.iexapis.com/stable/stock/fb/company?token=pk_6cad451b2ea543e4aa1b534419d7489f', 
-//         { json : true },
-//         (err, res, body) => {
-//             if(err) { return console.log(err);}
-//             if(res.statusCode === 200){
-//                 doneCallingCompany(body);
-//             };
-//     });
-// }
 
 
 // setting epress-handlebars Middleware
@@ -45,24 +23,95 @@ app.set('view engine', 'handlebars');
 
 // Set Handlebars Index GET route
 app.get('/',  (req, res) => {
-    call_stock_api( (doneCallingAPI) => {
+        console.log('Get / called');
         res.render('welcome',{
-            
-        });
     });
 
 });
 
+
 // Set Handlebars Index POST route
 app.post('/',  (req, res) => {
-    call_stock_api( (doneCallingAPI) => {
-        posted_stuff = req.body.stock_ticker
-        res.render('home',{
-            stock : doneCallingAPI,
-            posted_stuff : posted_stuff
-        });
-    },req.body.stock_ticker);
+    // Stocky will store the API response of getting stock details API
+    var stocky=null;
+    // comp will store the API response of getting company details API
+    var comp = null;
 
+    // Making an async parallel call for the two APIs - get stocks' details and get company details
+    async.parallel({ 
+        one: function(callback) { 
+              console.log('Calling get company details API');
+              request('https://cloud.iexapis.com/stable/stock/'+req.body.stock_ticker+'/company?token=pk_6cad451b2ea543e4aa1b534419d7489f',
+                         function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                            callback(null, body);
+                    } 
+                    else {
+                        // setting error as true
+                        callback(true, {});
+                    }
+      });
+              
+        },
+        two: function(callback) { 
+                console.log('Calling get company stock quote details API');
+                request('https://cloud.iexapis.com/stable/stock/'+req.body.stock_ticker+'/quote?token=pk_6cad451b2ea543e4aa1b534419d7489f',
+                    function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            callback(null, body);
+                        } 
+                        else {
+                            // setting error as true
+                            callback(true, {});
+                        }
+                });
+            },
+       
+        }, 
+        // optional callback
+        function(err, results) {
+          // 'results' is now equal to: {one: 1, two: 2, ..., something_else: some_value}
+          if(err){
+              console.log("Error occured while calling APIs: "+err);
+              renderErrorPage();
+              return;
+          }
+          
+          // if error has not occured
+          comp = results.one;
+          stocky = results.two;
+          renderPage();
+         
+        }
+      );
+
+      // function to render an error page when error occured due to given symbol not found
+      function renderErrorPage(){
+            console.log("renderErrorPage Method called");
+            console.log('stocky: '+stocky);
+            console.log('comp: '+comp);
+            // render notfound view and return
+            res.render('notfound');
+            return;
+      }
+
+      // function to render page with results of both APIs when no error has occured
+      function renderPage(){
+            console.log("renderPage Method called");
+            console.log('stocky: '+stocky);
+            console.log('comp: '+comp);
+
+            // Convert JSON String to JSON objects
+            const stockDetails = JSON.parse(stocky);
+            const companyDetails = JSON.parse(comp);
+        
+            // render home view with response from two APIs and return 
+            res.render('home',{
+                stock : stockDetails,
+                company : companyDetails
+            });
+        return;
+      }
 });
 
 
@@ -74,3 +123,11 @@ app.get('/about.html', function (req, res) {
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log("Server Listening on Port: "+PORT));
+
+
+
+
+
+
+
+
