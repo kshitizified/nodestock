3d5f5b23978c49408c29dacc8beee9bdcd2a3a62
kshitizified <kshitizkumar1995@gmail.com>
2020-05-30 15:19:12 +0530

    Adding third API async call for getting company logo and refactoring company card in home.handlebars

--- index.js
+++ index.js
@@ -33,9 +33,11 @@ app.get('/',  (req, res) => {
 // Set Handlebars Index POST route
 app.post('/',  (req, res) => {
     // Stocky will store the API response of getting stock details API
-    var stocky=null;
+    var stocky = null;
     // comp will store the API response of getting company details API
     var comp = null;
+    // logo will store response from the logo API
+    var logo = null;
 
     // Making an async parallel call for the two APIs - get stocks' details and get company details
     async.parallel({ 
@@ -66,6 +68,20 @@ app.post('/',  (req, res) => {
                         }
                 });
             },
+        
+        three: function(callback) { 
+            console.log('Calling get company logo from logo API');
+            request('https://cloud.iexapis.com/stable/stock/'+req.body.stock_ticker+'/logo?token=pk_6cad451b2ea543e4aa1b534419d7489f',
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                         callback(null, body);
+                    } 
+                    else {
+                        // setting error as true
+                        callback(true, {});
+                    }
+                });
+            },    
        
         }, 
         // optional callback
@@ -80,6 +96,7 @@ app.post('/',  (req, res) => {
           // if error has not occured
           comp = results.one;
           stocky = results.two;
+          logo = results.three;
           renderPage();
          
         }
@@ -90,6 +107,7 @@ app.post('/',  (req, res) => {
             console.log("renderErrorPage Method called");
             console.log('stocky: '+stocky);
             console.log('comp: '+comp);
+            console.log("logo: "+logo);
             // render notfound view and return
             res.render('notfound');
             return;
@@ -100,15 +118,17 @@ app.post('/',  (req, res) => {
             console.log("renderPage Method called");
             console.log('stocky: '+stocky);
             console.log('comp: '+comp);
-
+            console.log("logo: "+logo);
             // Convert JSON String to JSON objects
             const stockDetails = JSON.parse(stocky);
             const companyDetails = JSON.parse(comp);
+            const companyLogo = JSON.parse(logo);
         
             // render home view with response from two APIs and return 
             res.render('home',{
                 stock : stockDetails,
-                company : companyDetails
+                company : companyDetails,
+                logo : companyLogo
             });
         return;
       }
@@ -119,7 +139,7 @@ app.get('/about.html', function (req, res) {
     res.render('about');
 });
 
-// setting static folders
+// setting static folders path
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log("Server Listening on Port: "+PORT));
